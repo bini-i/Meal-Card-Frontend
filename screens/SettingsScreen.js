9f1a70cb603c8b02fbdd3f7596a32541e9c9e248
bini <bini@pop-os.localdomain>
2023-05-23 07:26:21 -0400

    update setting screen

--- screens/SettingsScreen.js
+++ screens/SettingsScreen.js
@@ -1,16 +1,60 @@
 import React, { useContext } from "react";
-import { Button, View, StyleSheet, Text } from "react-native";
+import { ScrollView, View, StyleSheet, Text } from "react-native";
 import { AuthContext } from "../context/AuthContext";
+import { theme } from "../core/theme";
+import { Divider, List } from 'react-native-paper';
+import Paragraph from "../components/Paragraph";
 
 export default function SettingsScreen() {
     const {user} = useContext(AuthContext)
     return (
       <View style={styles.container}>
-         <View style={styles.heroContainer}>
+         {/* <View style={styles.heroContainer}>
             <View style={{ flex: 1 }}>
                 <Text style={styles.heroTitle}>{user ? user.username : null}</Text>
             </View>
-        </View>
+        </View> */}
+        <Paragraph 
+          style={{
+            fontSize: 15, 
+            marginTop: 8, 
+            color: theme.colors.primary, 
+            borderLeftWidth: 2, 
+            borderColor: theme.colors.primary,
+            paddingLeft: 8,
+            marginBottom: 8,
+            alignSelf: "center"
+          }}
+        >
+            የዕለት መስተንግዶዎች መረጃ
+        </Paragraph>
+        <ScrollView>
+          {/* {[...Array(5).keys()].forEach(i=>{
+
+            return( */}
+              <View 
+                key={1}
+                style={{
+                  flexDirection: "row",
+                  alignItems: "center",
+                  backgroundColor: "white", 
+                  padding: 6, 
+                  marginBottom: 9,
+                  borderRadius: 7  
+                }}
+              >
+                <List.Icon  style={{color: theme.colors.primary}} icon={require('../assets/serve.png')} />
+                <View style={{flex: 1, flexDirection: "row" }}>
+                  <Text style={{fontWeight: "bold", color: theme.colors.primary}}>{ 2 + " ኩፖን" }</Text>
+                  <View style={{marginLeft: "auto", paddingHorizontal: 15 }}>
+                    <Text style={{color: "gray"}}>"date"</Text>
+                    <Text style={{color: "gray"}}>"time"</Text>
+                  </View>
+                </View>
+              </View>
+            {/* )
+          })} */}
+        </ScrollView>
       </View>
     );
 }
@@ -19,6 +63,7 @@ const styles = StyleSheet.create({
   container: {
     // justifyContent: "center",
     flex: 1,
+    padding: 17
   },
   text: {
     textAlign: "center",
